da022c0b46633f3b9c7e3cb556c8896e770f6b07
mahong <mahong@lianlianpay.com>
2022-02-14 15:43:52 +0800

    feat: dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="user"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
+  
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { ref, defineComponent } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ColumnList, { IColumnProps } from './components/ColumnList.vue';
+import GlobalHeader, { IUserProps } from './components/GlobalHeader.vue'
 
+let testData: IColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }
+]
+const userData: IUserProps = {
+  isLogin: true,
+  name: '马红',
+  id: 1
+}
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup(){
+    const list = ref(testData);
+    return {
+      list,
+      user: userData,
+    }
   }
 });
 </script>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col-4 mb-4" v-for="column in columnList" :key="column.id">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img  :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" >
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, PropType } from 'vue'
+export interface IColumnProps {
+  id: number
+  title: string
+  avatar?: string
+  description: string
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<IColumnProps[]>,
+      required: true
+    }
+  },
+  setup(props) {
+    // setup只在组件创建的时候执行一次
+    console.log('columnlist setup!!!!');
+    const columnList = computed(() => {
+      // 但里面调用的钩子函数和在外面用的钩子函数一样会在适当的时候执行，如computed每次props.list改变就执行
+      console.log('columnlist computed!!!!');
+      return props.list.map(column => {
+        if(!column.avatar) {
+          column.avatar = require('@/assets/logo.png')
+        }
+        return column;
+      })
+    })
+    return {
+      columnList
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
